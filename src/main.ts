f60d0e79218fd5b3ace6b5c507eec079c8b03884
Jarno Willems <jarno.willems2@student.odisee.be>
2024-12-13 17:32:18 +0100

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,5 @@
-import './style.css'
-// @ts-ignore
-import axios = require("axios")
-// @ts-ignore
-import cheerio = require('cheerio') // Import Cheerio for HTML manipulation
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>CLICK TE BUTTON</h1>
-    <div>
-        <h2>Blogpost van Arno zijn website</h2>
-        <p>Kijk hoe shit deze blogpost is, lelijk</p>
-        <a href="https://webmention-client.vercel.app/">Zie hier</a>
-    </div>
-    <div class="card">
-      <button id="button" type="button">Click me</button>
-    </div>
-    <a href="mailto:jarnowillems001@gmail.com" rel="me">john@gmail.com</a>
-  </div>
-`
+import axios from 'axios'
+import * as cheerio from 'cheerio'
 
 document.querySelector<HTMLButtonElement>('#button')!.addEventListener('click', async () => {
     const response = await axios.get('https://webmention-client.vercel.app/');
